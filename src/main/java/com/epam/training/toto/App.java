59eecea09a8decdf6db46c7a5bfc3e850e41c674
LindaRomfa <LindaRomfa@users.noreply.github.com>
2019-04-28 11:08:07 +0200

    commit

--- src/main/java/com/epam/training/toto/App.java
+++ src/main/java/com/epam/training/toto/App.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.epam.training.toto.domain.Hit;
 import com.epam.training.toto.domain.Outcome;
@@ -20,28 +22,18 @@ import com.epam.training.toto.domain.Round;
 public class App {
     private static Scanner sc = new Scanner(System.in);
     private static LocalDate in;
-    private static String StrIn;
     private static int counter = 0;
-    private static List<String> OutcomeList = new ArrayList<>();
+    private static List<String> outcomeList = new ArrayList<>();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.d.");
-    private static boolean DateInBoolean, OutcomelistInBoolean;
 
     public static void main(String[] args) throws IOException {
         List<Round> rounds = readRound();
-
         maxPrize(rounds);
-
         statistics(rounds);
-
-        DateInBoolean = DateIn();
-        OutcomelistInBoolean = OutcomeListIn();
-            sc.close();
-         HitCounter(rounds, DateInBoolean, OutcomelistInBoolean);
-
-
-
+        dateIn();
+        OutcomeListIn();
+        hitCounter(rounds);
     }
-
     private static List<Round> readRound() throws IOException {
         List<Round> rounds = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("D:/Epam/Training/src/main/resources/toto.csv"))) {
@@ -50,17 +42,11 @@ public class App {
                 String[] line = nextLine.split(";");
                 Round round = creatRound(line);
                 rounds.add(round);
-
             }
-
         }
         return rounds;
     }
-
     private static Round creatRound(String[] nextLine) {
-
-        List<Hit> hits = new ArrayList<>();
-        List<Outcome> outcomes = new ArrayList<>();
         Round round = new Round();
         round.setYear(Integer.parseInt(nextLine[0]));
         round.setWeek(Integer.parseInt(nextLine[1]));
@@ -73,46 +59,40 @@ public class App {
         } else {
             round.setDate(LocalDate.parse("2015-01-01"));
         }
-        // Hit
-        Hit hit = creatHit(nextLine[4], nextLine[5], 14);
-        hits.add(hit);
-        hit = creatHit(nextLine[6], nextLine[7], 13);
-        hits.add(hit);
-        hit = creatHit(nextLine[8], nextLine[9], 12);
-        hits.add(hit);
-        hit = creatHit(nextLine[10], nextLine[11], 11);
-        hits.add(hit);
-        hit = creatHit(nextLine[12], nextLine[13], 10);
-        hits.add(hit);
-        round.setHits(hits);
-
-        // Outcomes
-        for (int i = 14; i < 28; i++) {
-            outcomes.add(creatOutcome(nextLine[i]));
-
-        }
-        round.setOutcomes(outcomes);
-
+        // Created the hits
+        round.setHits(creatHitList(nextLine));
+        // created the outcomes
+        round.setOutcomes(creatOutcomeList(nextLine));
         return round;
     }
-
-    private static Hit creatHit(String line1, String line2, int numb) {
-        Hit hit = new Hit();
-        String line = line2.substring(0, line2.length() - 3);
-        String[] arr = line.split(" ");
-        StringBuilder br = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            br.append(arr[i]);
+    private static List<Hit> creatHitList(String[] nextLine){
+        List<Hit> hits = new ArrayList<>();
+        Hit hit;
+        int hitCountNumber = 14;
+        for(int i = 4; i < 13;i += 2){
+            hit =  creatHit(nextLine[i], nextLine[i+1], hitCountNumber);
+            hits.add(hit);
+            hitCountNumber--;
         }
-        line = br.toString();
-        hit.setHitCount(numb);
-        hit.setNumberOfWagers(Integer.parseInt(line1));
-        hit.setPrize(Integer.parseInt(line));
+        return hits;
+    }
+    private static Hit creatHit(String numberOfWager, String prize, int hitCount) {
+        Hit hit = new Hit();
+        prize = prize.replace(" ","").replace("Ft","");
+        hit.setHitCount(hitCount);
+        hit.setNumberOfWagers(Integer.parseInt(numberOfWager));
+        hit.setPrize(Integer.parseInt(prize));
         return hit;
     }
-
+    private static List<Outcome> creatOutcomeList(String[] nextLine){
+        List<Outcome> outcomes = new ArrayList<>();
+        for (int i = 14; i < 28; i++) {
+            outcomes.add(creatOutcome(nextLine[i]));
+        }
+        return outcomes;
+    }
     private static Outcome creatOutcome(String line) {
-        Outcome outcome = null;
+        Outcome outcome;
         if (line.equals("1") || line.equals("+1")) {
             outcome = Outcome._1;
         } else if (line.equals("2") || line.equals("+2")) {
@@ -123,63 +103,62 @@ public class App {
         return outcome;
     }
 
-    private static boolean DateIn() {
+    private static void dateIn() {
         System.out.println("Scanner:");
         System.out.print("Enter date: ");
-        try {
-            in = LocalDate.parse(sc.nextLine(),FORMATTER);
-        } catch (DateTimeParseException e) {
-            System.err.println(e);
-            return false;
-        } catch(Exception e){
-            System.err.println(e);
-        }
-        return true;
+        boolean incorrectDateInput ;
+        do {
+            incorrectDateInput = false;
+            try {
+                in = LocalDate.parse(sc.nextLine(), FORMATTER);
+            } catch (DateTimeParseException e) {
+                incorrectDateInput = true;
+                System.out.println("Incorrect date, please try again: ");
+            }
+        }while(incorrectDateInput);
+
     }
 
-    private static boolean OutcomeListIn() {
+    private static void OutcomeListIn() {
         System.out.print("Enter outcomes: ");
         System.out.println("");
-        StrIn = sc.nextLine();
-        OutcomeList = Arrays.asList(StrIn.split(""));
-        if (OutcomeList.size() != 14) {
-            System.out.println("Incorrect input!");
-            return false;
+        String outcomesIn;
+        boolean incorrectOutcomeInput;
+        do{
+            incorrectOutcomeInput = false;
+            outcomesIn = sc.nextLine();
+            outcomeList = Arrays.asList(outcomesIn.split(""));
+        if (outcomeList.size() != 14) {
+            incorrectOutcomeInput = true;
         }
-        for (String o : OutcomeList) {
-            if (!(o.equals("1")) && !(o.equals("2")) && !(o.equals("x"))) {
-                System.out.println("Incorrect input!");
-                return false;
+
+        for (String outcomes : outcomeList) {
+            if (!(outcomes.equals("1")) && !(outcomes.equals("2")) && !(outcomes.equals("x"))) {
+                incorrectOutcomeInput = true;
             }
         }
-        return true;
+        if(incorrectOutcomeInput){
+            System.out.println("Incorrect outcome, please try again: ");
+        }
+        }while(incorrectOutcomeInput);
+        sc.close();
     }
-
-    private static void HitCounter(List<Round> rounds,boolean DateInBoolean, boolean OutcomelistInBoolean) {
+    private static void hitCounter(List<Round> rounds) {
         int i = 0;
-        if (DateInBoolean && OutcomelistInBoolean) {
-                    List<Outcome> outcomes = rounds.stream().filter(x -> x.getDate().equals(in)).map(Round::getOutcomes)
-                            .flatMap(z -> z.stream()).collect(Collectors.toList());
-                    for (Outcome outcome : outcomes) {
-                        if ((OutcomeList.get(i).equals("1") && outcome.equals(Outcome._1))) {
-                            counter++;
-                        }
-                        if(OutcomeList.get(i).equals("2") && outcome.equals(Outcome._2)) {
-                            counter++;
-                        }
-                        if(OutcomeList.get(i++).equals("x") && outcome.equals(Outcome.x)) {
-                            counter++;
-                        }
-                    }
-                    List<Hit> hits = rounds.stream().filter(x -> x.getDate().equals(in)).map(Round::getHits)
-                            .flatMap(z -> z.stream()).collect(Collectors.toList());
-                    for(Hit hit : hits) {
-                        if(hit.getHitCount() == counter) {
-                            System.out.println("Result: hits: " + hit.getHitCount() + ", amount: " + hit.getPrize() + " Ft");
-                        }
-                    }
-        }else {
-            System.out.println("Incorrect input!");
+        List<Outcome> outcomes = rounds.stream().filter(x -> x.getDate().equals(in)).map(Round::getOutcomes)
+                .flatMap(z -> z.stream()).collect(Collectors.toList());
+        for (Outcome outcome : outcomes) {
+            if ((outcomeList.get(i).equals(outcome.getValue()))) {
+                counter++;
+            }
+            i++;
+        }
+        List<Hit> hits = rounds.stream().filter(x -> x.getDate().equals(in)).map(Round::getHits)
+                .flatMap(z -> z.stream()).collect(Collectors.toList());
+        for(Hit hit : hits) {
+            if(hit.getHitCount() == counter) {
+                System.out.println("Result: hits: " + hit.getHitCount() + ", amount: " + hit.getPrize() + " Ft");
+            }
         }
     }
     private static void maxPrize(List<Round> rounds){
@@ -190,19 +169,22 @@ public class App {
         System.out.println(decimalFormat.format(MaxPrize));
         System.out.print ("Statistic: ");
     }
-
     private static void statistics(List<Round> rounds){
+        Supplier<Stream> outcomesCollect =() ->  rounds.stream().map(Round::getOutcomes).flatMap(l -> l.stream());
+        double allCount = outcomesCollect.get().count();
+        double firstCount = outcomesCollect.get().filter(i -> i == Outcome._1)
+                .count();
+        System.out.print("team #1 won: " + outcomePercentage(firstCount,allCount) + "%,");
+        double secondCount =outcomesCollect.get().filter(i -> i == Outcome._2)
+                .count();
+        System.out.print(" team #2 won: " + outcomePercentage(secondCount,allCount) + "%,");
+        double drawCount = outcomesCollect.get().filter(i -> i == Outcome.x)
+                .count();
+        System.out.println(" draw: " + outcomePercentage(drawCount,allCount) + "%");
+    }
+    private static String outcomePercentage(double part,double all){
         DecimalFormat countFormat = new DecimalFormat("##.##");
-        double allCount = rounds.stream().map(Round::getOutcomes).flatMap(l -> l.stream()).count();
-        double FirstCount = rounds.stream().map(Round::getOutcomes).flatMap(l -> l.stream()).filter(i -> i == Outcome._1)
-                .count();
-        System.out.print("team #1 won: " + countFormat.format(FirstCount / allCount * 100 ) + "%,");
-        double SecondCount = rounds.stream().map(Round::getOutcomes).flatMap(l -> l.stream()).filter(i -> i == Outcome._2)
-                .count();
-        System.out.print(" team #2 won: " + countFormat.format(SecondCount / allCount * 100 ) + "%,");
-        double DrawCount = rounds.stream().map(Round::getOutcomes).flatMap(l -> l.stream()).filter(i -> i == Outcome.x)
-                .count();
-        System.out.println(" draw: " + countFormat.format(DrawCount / allCount * 100 ) + "%");
+        return countFormat.format(part / all * 100 );
     }
 
 }
--- src/main/java/com/epam/training/toto/domain/Outcome.java
+++ src/main/java/com/epam/training/toto/domain/Outcome.java
@@ -2,20 +2,18 @@ package com.epam.training.toto.domain;
 
 public enum Outcome {
 
-    _1{
-        public String toString() {
-            return "1";
-        }
-    },
-    _2{
-        public String toString() {
-            return "2";
-        }
-    },
-    x{
-        public String toString() {
-            return "x";
-        }
-    };
+    _1("1"),
+    _2("2"),
+    x("x");
 
+    private String value;
+    Outcome(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
+    public void setValue(String value){
+        this.value = value;
+    }
 }

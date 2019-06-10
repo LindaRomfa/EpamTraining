e21a6ef427a723a5ed1478d8127aebd0a9e41f53
LindaRomfa <LindaRomfa@users.noreply.github.com>
2019-06-10 12:25:04 +0200

    add unit tests and repair the code

--- src/main/java/com/epam/training/sportsbetting/ui/IO.java
+++
@@ -1,8 +0,0 @@
-package com.epam.training.sportsbetting.ui;
-
-import com.epam.training.sportsbetting.domain.Player;
-import java.math.BigDecimal;
-
-public interface IO {
-    BigDecimal readWagerAmount();
-}

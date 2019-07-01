4bfe4f6ab0397d5631389b8d87d7b32af67d4639
LindaRomfa <LindaRomfa@users.noreply.github.com>
2019-07-01 20:24:52 +0200

    implements the second test

--- src/main/java/com/epam/training/gameoflife/DefaultBoard.java
+++ src/main/java/com/epam/training/gameoflife/DefaultBoard.java
@@ -25,17 +25,68 @@ public class DefaultBoard implements Board {
     public void insertCell(Coordinate coordinate) {
         if (!coordinates.contains(coordinate)) {
             coordinates.add(coordinate);
-            this.isAlive(coordinate);
         }
     }
 
     @Override
     public boolean isAlive(Coordinate coordinate) {
-            if (coordinates.contains(coordinate)) {
+        int neighbor = neighborCounter(coordinate);
+        if (coordinates.contains(coordinate)) {
+            if (neighbor == 2 || neighbor == 3) {
                 return true;
             }
+            return true;
+        }
+
 
         return false;
+    }
+
+    private int neighborCounter(Coordinate coordinate) {
+        int counter = 0;
+        for (Coordinate coordinate1 : coordinates) {
+            if ((coordinate1.getPositionX() == ((coordinate.getPositionX()) - 1))) {
+                counter = counter + positionYCounter(coordinate1, coordinate);
+            }
+            if (coordinate1.getPositionX() == (coordinate.getPositionX())) {
+
+                counter = counter + positionYMinusOne(coordinate1, coordinate);
+
+                counter = counter + positionYPlusOne(coordinate1, coordinate);
+
+            }
+            if ((coordinate1.getPositionX() == (coordinate.getPositionX() + 1))) {
+                counter = counter + positionYCounter(coordinate1, coordinate);
+            }
+
+        }
+        return counter;
+    }
+
+    private int positionYCounter(Coordinate coordinate1, Coordinate coordinate) {
+        int counter = 0;
+        counter = counter + positionYMinusOne(coordinate1, coordinate);
+        if (coordinate1.getPositionY() == (coordinate.getPositionY())) {
+            counter++;
+        }
+        counter = counter + positionYPlusOne(coordinate1, coordinate);
+        return counter;
+    }
+
+    private int positionYMinusOne(Coordinate coordinate1, Coordinate coordinate) {
+        int counter = 0;
+        if ((coordinate1.getPositionY() == (coordinate.getPositionY() - 1))) {
+            counter++;
+        }
+        return counter;
+    }
+
+    private int positionYPlusOne(Coordinate coordinate1, Coordinate coordinate) {
+        int counter = 0;
+        if ((coordinate1.getPositionY() == (coordinate.getPositionY() + 1))) {
+            counter++;
+        }
+        return counter;
     }
 }
 
